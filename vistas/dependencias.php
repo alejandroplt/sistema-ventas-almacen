5b333f0828bf60c1023415a63653e1245d6743cd
Alejandro Platas <alejandro.platas.02@gmail.com>
2021-09-07 00:43:22 -0500

    conexion bd

--- vistas/dependencias.php
+++ vistas/dependencias.php
@@ -7,6 +7,9 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="librerias/alertifyjs/css/alertify.css">
+    <link rel="stylesheet" type="text/css" href="librerias/alertifyjs/css/themes/default.css">
+    <link rel="stylesheet" type="text/css" href="librerias/select2/css/select2.css">
 
     <title>Hello, world!</title>
   </head>
@@ -23,5 +26,9 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
     -->
+    <script src="librerias/jquery-3.2.1.min.js"></script>
+    <script src="librerias/alertifyjs/alertify.js"></script>
+    <script src="librerias/select2/js/select2.js"></script>
+
     </body>
 </html>
---
+++ vistas/menu.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Ventas</title>
+        
+        <?php
+
+            require_once "dependencias.php";
+            
+        ?>
+	</head>
+	<body>
+
+	</body>
+</html>
