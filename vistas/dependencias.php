f83018910ba46d89a3f0a51f9f8104c4f117dd13
Alejandro Platas <alejandro.platas.02@gmail.com>
2021-09-21 01:21:22 -0500

    menu

--- vistas/dependencias.php
+++ vistas/dependencias.php
@@ -1,34 +1,14 @@
-<DOCTYPE html>
-<html lang="es">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="librerias/alertifyjs/css/alertify.css">
-    <link rel="stylesheet" type="text/css" href="librerias/alertifyjs/css/themes/default.css">
-    <link rel="stylesheet" type="text/css" href="librerias/select2/css/select2.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/themes/default.css">
+<link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../librerias/select2/css/select2.css">
+<link rel="stylesheet" type="text/css" href="../css/menu.css">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
-    -->
-    <script src="librerias/jquery-3.2.1.min.js"></script>
-    <script src="librerias/alertifyjs/alertify.js"></script>
-    <script src="librerias/select2/js/select2.js"></script>
-
-    </body>
-</html>
+<script src="../librerias/jquery-3.2.1.min.js"></script>
+<script src="../librerias/alertifyjs/alertify.js"></script>
+<script src="../librerias/bootstrap/js/bootstrap.js"></script>
+<script src="../librerias/select2/js/select2.js"></script>
+<script src="../js/funciones.js"></script>
--- vistas/inicio.php
+++ vistas/inicio.php
@@ -3,7 +3,6 @@
     session_start();
 
     if(isset($_SESSION['usuario'])){
-        echo $_SESSION['usuario'];
 
 ?>
 
